4f73dcb1b4af907abf823ecffee45969bc2c6430
16010-54-07 <sam@google.cojordan.gengoux.afpa@gmail.com>
2023-06-08 12:38:15 +0200

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+use Classes\Models\Brand;
+use Classes\Models\BrandManager;
+
+require_once 'classes\Models\Brand.php';
+require_once 'classes\Models\BrandManager.php';
+require_once 'autoload.php';
+
+
+$brand = new Brand();
+$brand->hydrate(['name'=>'une marque en ECF']);
+
+//var_dump($brand->isNew());
+
+//var_dump($brand);
+$manager = new BrandManager();
+//var_dump($manager->findAll());
+//var_dump($manager->find(2));
+var_dump($manager->create($brand));
